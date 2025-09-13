4c5d6e860faf2df856cf663c88cf7d2f0725376f
missage999 <2270656968@qq.com>
2025-09-13 22:45:04 +0800

    补充智能指针,并调整项目结构,添加.gitignore

--- task2/smart_ptr.cpp
+++ task2/smart_ptr.cpp
@@ -5,43 +5,54 @@
 #include <iostream>
 #include <memory>
 
-class Robot {
+class Robot
+{
 public:
-    Robot()  { std::cout << "Robot constructed\n"; }
+    Robot() { std::cout << "Robot constructed\n"; }
     ~Robot() { std::cout << "Robot destroyed\n"; }
     void speak() const { std::cout << "Beep boop\n"; }
 };
 
 // TODO 1：返回裸指针，调用者易忘 delete
-Robot* createRawRobot() {
+Robot *createRawRobot()
+{
     return new Robot();
 }
 
 // TODO 2：返回 unique_ptr，独占所有权
-std::unique_ptr<Robot> createUniqueRobot() {
-    return std::make_unique<Robot>();   // 已 OK
+std::unique_ptr<Robot> createUniqueRobot()
+{
+    return std::make_unique<Robot>(); // 已 OK
 }
 
 // TODO 3：返回 shared_ptr，共享所有权
-std::shared_ptr<Robot> createSharedRobot() {
+std::shared_ptr<Robot> createSharedRobot()
+{
     // 请用 std::make_shared 实现
     return std::make_shared<Robot>();
 }
 
-int main() {
+int main()
+{
     std::cout << "=== raw pointer ===\n";
     // 调用 createRawRobot，使用完后手动 delete
-    auto ptr=createRawRobot();
+    auto ptr = createRawRobot();
     ptr->speak();
     delete ptr;
 
     std::cout << "\n=== unique_ptr ===\n";
-    auto u = createUniqueRobot();
-    u->speak();
+    auto u1 = createUniqueRobot();
+    u1->speak();
+    // auto u2=u1;编译错误，unique_ptr不能直接拷贝，需要move，将所有权转移
+    auto u2 = std::move(u1);
 
     std::cout << "\n=== shared_ptr ===\n";
     auto s1 = createSharedRobot();
-    auto s2 = s1;               // 共享
+    // 显示计数
+    std::cout << s1.use_count() << std::endl;
+    auto s2 = s1; // 共享
+    std::cout << s1.use_count() << std::endl;
+    std::cout << s2.use_count() << std::endl;
     s1->speak();
 
     return 0;
